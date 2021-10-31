4eafc4c25ed977bc340c43165e34d8670273047e
ikhmal2 <ax.ikhmal.145713@gmail.com>
2021-11-01 01:22:47 +0800

    add into funtions and trying to add loop

--- pyPasssManagerV2.py
+++ pyPasssManagerV2.py
@@ -1,19 +1,49 @@
 import random
 import string
+import os
 
 chars = string.printable
+website = ""
+password = ""
 
-while True:
-    website = input("What is the name of the web app?: ")
-    password_len = int(input("How many characters do you want in your password?: "))
-    password = ""
-    for x in range(0,password_len):
-        password_char = random.choice(chars)
-        password = password + password_char
-    break
+def createPass():
+    global website
+    global password
 
-print("Your creditantials are as follows: ")
-print("----------------------------------")
-print("Website:",website)
-print("Password:",password)
+    while True:
+        website = input("What is the name of the web app?: ")
+        password_len = int(input("How many characters do you want in your password?: "))
+        password = ""
+        for x in range(0,password_len):
+            password_char = random.choice(chars)
+            password = password + password_char
+        
+        answer = input("Do you want to add more passwords? :")
+        answer = answer.lower()
+        if(answer == "yes"):
+            continue
+        else:
+            break
 
+def output():
+    print("Your creditantials are as follows: ")
+    print("----------------------------------")
+    print("Website:",website)
+    print("Password:",password)
+
+def checkFileExist():
+    if os.path.exists("passwords.txt"):
+        pass
+    else:
+        file = open("passwords.txt","w")
+        file.close
+
+def appendNew(website,password):
+    with open("passwords.txt","a") as file:
+        file.write("Website: " + website)
+        file.write(" Password: " + password + "\n")
+
+createPass()
+checkFileExist()
+appendNew(website,password)
+output()
